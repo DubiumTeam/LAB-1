53c27e0944f50638546a15cb255b59f8822bca76
SirGlitches <dante.hedra@gmail.com>
2020-05-20 14:34:12 +0200

    update

--- src/utils.c
+++ src/utils.c
@@ -4,12 +4,9 @@
  * TODO: Convert upper case to lower case
  * Modify the given string mapping all characters in [A-Z] to [a-z].
 */
-void str_to_lowercase(char *s) {
-    int i;
-    for(i=0; str[i]!='\0'; i++)
-    {
-        if(str[i]>='A' && str[i]<='Z')
-        {
+void str_to_lowercase(char *s){
+    for(int i=0; str[i]!='\0'; i++){
+        if(str[i]>='A' && str[i]<='Z'){
             str[i] = str[i] + 32;
         }
     }
